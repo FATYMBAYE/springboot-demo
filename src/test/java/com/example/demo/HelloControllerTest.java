d383f6d332bea6af4f85ce1c8c62ba035a58dae0
FATYMBAYE <108554588+FATYMBAYE@users.noreply.github.com>
2025-06-24 19:19:57 +0200

    modification hierarchie dossier

--- src/test/java/com/example/demo/HelloControllerTest.java
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -1,9 +1,8 @@
-package com.example;
+package com.example.demo;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
-import com.example.demo.HelloController;
 
 
 class HelloControllerTest {
